926688e7a1e3ab6648e7e45aef2387ad795964c3
Dalton Eggleston <egglestondalton@yahoo.com>
2022-05-31 11:24:42 -0700

    created a class

--- User.py
+++ User.py
@@ -1,1 +1,20 @@
-# your User class goes here
+class User:
+    def __init__(self, name, email, drivers_license, age):
+        self.name = name
+        self.email = email
+        self.drivers_license = drivers_license
+        self.age = age
+        print("Intizializing")
+    def __str__(self):
+        print(f"My name is {self.name} and I am {self.age} years old")
+    
+    # 
+dalton = User("Dalton", "egglestondalton@yahoo.com", "WDLA12324AD", 22)
+michael = User("Michael", "feaW@gmail.com", "SF223AWDAD3", 22)
+Christopher = User("Christopher", 'giadawd@gmail.com', "WDA2323ADFA", 44)
+print(dalton)
+
+
+
+
+    
